e2653b01780fb9fb65bb3a7df7365e9944c1b44a
Lucas Willian <lucas.willianxd@gmail.com>
2025-11-17 20:26:46 -0300

    feat(home): cria página Home

---
+++ src/pages/PageHome.tsx
@@ -0,0 +1,12 @@
+import { Container } from "../components/Container";
+import { TasksSummary } from "../core-components/TasksSummary";
+
+export function PageHome() {
+  return (
+    <Container as="article" className="space-y-3">
+      <header className="flex items-center justify-between">
+        <TasksSummary />
+      </header>
+    </Container>
+  );
+}
